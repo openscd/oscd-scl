72be8830401f8d1f5a4252b1ecb4de5617723073
Jakob Vogelsang <jakob-vogelsang@posteo.de>
2023-01-27 17:19:33 +0100

    feat: add getReference
    SCL schemas define most elements within sequences. This function helps to determin in insert functions where to add the new child element within a aéxisting parent

--- utils/tags.ts
+++ utils/tags.ts
@@ -845,3 +845,32 @@ export function childTags(tagName: string): string[] {
 
   return tags[tagName].children;
 }
+
+/** @returns Reference for new [[`tag`]] child within [[`parent`]]  or `null` */
+export function getReference(parent: Element, tag: string): Element | null {
+  if (!isSCLTag(tag)) return null;
+
+  const parentTag = parent.tagName;
+  const children = Array.from(parent.children);
+
+  if (
+    parentTag === 'Services' ||
+    parentTag === 'SettingGroups' ||
+    !isSCLTag(parentTag)
+  )
+    return children.find(child => child.tagName === tag) ?? null;
+
+  const sequence = tags[parentTag].children;
+  let index = sequence.findIndex(element => element === tag);
+
+  if (index < 0) return null;
+
+  let nextSibling: Element | undefined;
+  while (index < sequence.length && !nextSibling) {
+    // eslint-disable-next-line no-loop-func
+    nextSibling = children.find(child => child.tagName === sequence[index]);
+    index += 1;
+  }
+
+  return nextSibling ?? null;
+}
